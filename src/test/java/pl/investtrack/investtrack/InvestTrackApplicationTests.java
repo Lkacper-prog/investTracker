2a3b9391b60552e6edf9885ac8e82b43b263f357
Kacper <kacper.lapinski21@gmail.com>
2025-11-23 01:30:11 +0100

    creat project

---
+++ src/test/java/pl/investtrack/investtrack/InvestTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.investtrack.investtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
