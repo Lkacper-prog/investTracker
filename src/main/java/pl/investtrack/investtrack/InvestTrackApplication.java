19f681f20ef223279f1c327e3400505474606bf4
Kacper <kacper.lapinski21@gmail.com>
2025-11-23 01:32:08 +0100

    adding properties database

--- src/main/java/pl/investtrack/investtrack/InvestTrackApplication.java
+++ src/main/java/pl/investtrack/investtrack/InvestTrackApplication.java
@@ -1,13 +1,25 @@
 package pl.investtrack.investtrack;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @SpringBootApplication
 public class InvestTrackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InvestTrackApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InvestTrackApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+          log.info("Hello World!");
+        };
+    }
 }
+
